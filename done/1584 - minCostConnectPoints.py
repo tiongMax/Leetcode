a7c76ed87bbf6c5b0bfa07af7bae06000990bf25
tiongMax <maxtiongcs16@gmail.com>
2024-06-20 22:59:48 +0800

    2 questions on 20/6/2024

--- done/1584 - minCostConnectPoints.py
+++ done/1584 - minCostConnectPoints.py
@@ -3,6 +3,7 @@
 import heapq
 from typing import List
 
+# Approach 1: Prims's
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         points = [tuple(p) for p in points]
@@ -35,5 +36,56 @@ class Solution:
 
         return mst
 
+# Approach 2: Kruskal's
 
+class UnionFind:
+    def __init__(self, edges):
+        self.par = {}
+        self.rank = {}
+        for e in edges:
+            self.par[e] = e
+            self.rank[e] = 0
 
+    def find(self, n):
+        p = self.par[n]
+        while p != self.par[p]:
+            self.par[p] = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += 1
+        return True
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        minHeap = []
+        for i in range(len(points)):
+            for j in range(len(points)):
+                if i == j: 
+                    continue
+                p1, p2 = tuple(points[i]), tuple(points[j])
+                dist = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+                heapq.heappush(minHeap, [dist, p1, p2])
+
+        uf = UnionFind([tuple(p) for p in points])
+        mst = 0
+        res = 0
+        while mst < len(points) - 1:
+            w, s, d = heapq.heappop(minHeap)
+            if not uf.union(s, d):
+                continue
+            res += w
+            mst += 1
+
+        return res
