541baff2b01c582572ce74551e6e31b9bc382321
tiongMax <maxtiongcs16@gmail.com>
2024-08-09 23:44:27 +0800

    1 ques on 9/8/2024

---
+++ done/840 - numMagicSquaresInside.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/magic-squares-in-grid/
+
+from typing import List
+
+class Solution:
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        def magic(r, c):
+            # Ensure values are from 1 to 9 and distinct
+            values = set()
+            for i in range(r, r + 3):
+                for j in range(c, c + 3):
+                    if grid[i][j] in values or not (1 <= grid[i][j] <= 9):
+                        return 0
+                    values.add(grid[i][j])
+
+            # Check rows
+            for i in range(r, r + 3):
+                if sum(grid[i][c:c+3]) != 15:
+                    return 0
+
+            # Check columns
+            for i in range(c, c + 3):
+                if grid[r][i] + grid[r+1][i] + grid[r+2][i] != 15:
+                    return 0
+
+            # Check diagonals
+            if grid[r][c] + grid[r+1][c+1] + grid[r+2][c+2] != 15 or \
+               grid[r][c+2] + grid[r+1][c+1] + grid[r+2][c] != 15:
+                return 0
+
+            return 1
+        
+        res = 0
+        for r in range(ROWS - 2):
+            for c in range(COLS - 2):
+                res += magic(r, c)
+        
+        return res
