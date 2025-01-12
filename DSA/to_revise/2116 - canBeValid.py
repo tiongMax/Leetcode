2d8a6bd75a4de4cb20623e4e07ab738e3f8401a6
tiongMax <maxtiongcs16@gmail.com>
2025-01-12 22:10:10 +0800

    Add 2116 to to_revise at 12/1/2025

---
+++ DSA/to_revise/2116 - canBeValid.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/check-if-a-parentheses-string-can-be-valid/
+
+class Solution:
+    def canBeValid(self, s: str, locked: str) -> bool:
+        if len(s) % 2 != 0:
+            return False
+
+        free, brackets = [], []
+        for i, ch in enumerate(s):
+            if locked[i] == "0":
+                free.append(i)
+                continue
+
+            if ch == "(":
+                brackets.append(i)
+                continue
+
+            # chr == ')'
+            if brackets:
+                brackets.pop()
+                continue
+
+            # brackets count == 0
+            if free:
+                free.pop()
+                continue
+
+            return False
+
+        while brackets and free and brackets[-1] < free[-1]:
+            brackets.pop()
+            free.pop()
+
+        if brackets or (free and len(free) % 2 == 1):
+            return False
+
+        return True
