78cb0c3d19704cd9768c0ebea70e8504ae9b539f
tiongMax <maxtiongcs16@gmail.com>
2025-01-13 23:06:17 +0800

    Add 3223 to done at 13/1/2025

---
+++ DSA/done/3223 - minimumLength.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/minimum-length-of-string-after-operations/
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        s_set = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+        ans = 0
+        for ch in s_set:
+            count = s.count(ch)
+            if count & 1:
+                ans += 1
+            elif count != 0:
+                ans += 2
+        return ans
