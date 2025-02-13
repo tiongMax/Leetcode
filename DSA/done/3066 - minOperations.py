3e9fa94d982896723e4b34866779d44b006bf96b
tiongMax <maxtiongcs16@gmail.com>
2025-02-13 21:51:53 +0800

    Add 3066 to done

---
+++ DSA/done/3066 - minOperations.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/minimum-operations-to-exceed-threshold-value-ii/
+
+from typing import List
+import heapq
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+
+        num_operations = 0
+        while nums[0] < k:
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            heapq.heappush(nums, min(x, y) * 2 + max(x, y))
+
+            num_operations += 1
+
+        return num_operations
