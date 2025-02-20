5a7f964ced98562ad565340644f3d1dd46d5f45f
tiongMax <maxtiongcs16@gmail.com>
2025-02-20 22:45:17 +0800

    Add 1980 to to_revise

---
+++ DSA/1980 - findDifferentBinaryString.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/find-unique-binary-string/
+
+from typing import List
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        def generate(curr):
+            if len(curr) == n:
+                if curr not in nums:
+                    return curr
+                
+                return ""
+            
+            add_zero = generate(curr + "0")
+            if add_zero:
+                return add_zero
+
+            return generate(curr + "1")
+
+        n = len(nums)
+        nums = set(nums)
+        return generate("")
