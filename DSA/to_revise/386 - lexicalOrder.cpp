8603df2c5249de1b3578eb7191abae719bc80d37
tiongMax <maxtiongcs16@gmail.com>
2025-06-08 23:43:48 +0800

    Add 386 to to_revise

---
+++ DSA/to_revise/386 - lexicalOrder.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/lexicographical-numbers/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> lexicographicalNumbers;
+        for (int start { 1 }; start <= 9; ++start) {
+            generateLexicalNumbers(start, n, lexicographicalNumbers);
+        }
+        return lexicographicalNumbers;
+    }
+
+private:
+    void generateLexicalNumbers(int currentNumber, int limit, vector<int>& result) {
+        if (currentNumber > limit) return;
+
+        result.push_back(currentNumber);
+        for (int nextDigit { 0 }; nextDigit <= 9; ++nextDigit) {
+            int nextNumber { currentNumber * 10 + nextDigit };
+            generateLexicalNumbers(nextNumber, limit, result);
+        }
+    }
+};
---
+++ DSA/to_revise/386 - lexicalOrder.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/lexicographical-numbers/
+
+from typing import List
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        lexicographical_numbers = []
+        for start in range(1, 10):
+            self._generate_lexical_numbers(start, n, lexicographical_numbers)
+        return lexicographical_numbers
+
+    def _generate_lexical_numbers(self, current_number: int, limit: int, result: List[int]):
+        if current_number > limit:
+            return
+        result.append(current_number)
+
+        for next_digit in range(10):
+            next_number = current_number * 10 + next_digit
+            self._generate_lexical_numbers(next_number, limit, result)
