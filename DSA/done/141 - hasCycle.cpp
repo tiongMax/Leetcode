6a796fe975ac14ebc3543e4ce4bcb04481d6ae58
tiongMax <maxtiongcs16@gmail.com>
2026-01-18 15:27:59 +0800

    Add 141, 237, 876 to DSA/done

---
+++ DSA/done/141 - hasCycle.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/linked-list-cycle/description/
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};
---
+++ DSA/done/876 - middleNode.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/
+
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        s, f = head, head
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+        return s
