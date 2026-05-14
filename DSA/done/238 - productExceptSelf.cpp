febb916bfdd9d850332a0b6f876cec1b7a81381e
tiongMax <maxtiongcs16@gmail.com>
2026-05-14 15:54:41 +0800

    Add 238 to DSA/done

---
+++ DSA/done/238 - productExceptSelf.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/product-of-array-except-self/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> res(nums.size(), 1);
+        int prefix{1}, postfix{1};
+        for (int i = 0; i < nums.size(); i++) {
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+};
---
+++ DSA/done/238 - productExceptSelf.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/product-of-array-except-self/
+
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+        prefix = postfix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
